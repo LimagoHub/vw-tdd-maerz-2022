f7506ae966a30bc60f715d1a17b8b885a2ff52e3
Jo Wagner <jowagner@limago.de>
2022-03-31 08:35:58 +0200

    print eingebaut

--- Nimmspiel/Nimmspiel/takegame_impl.h
+++ Nimmspiel/Nimmspiel/takegame_impl.h
@@ -9,7 +9,7 @@ class takegame_impl: public game
 	int stones;
 	int move;
 
-	bool is_gameover()
+	bool is_gameover() const
 	{
 		return stones < 1;
 	}
@@ -28,48 +28,70 @@ class takegame_impl: public game
 		
 		
 		move = moves[stones % 4];
-		std::cout << "Computer nimmt " << move << " Steine." << std::endl;
-		
+		print("Computer nimmt " + std::to_string(move) + " Steine.");
 		terminate_move("Computer");
 	}
+
+	
 
 	void spielerzug()
 	{
 		if (is_gameover()) return;
-		
-		
-		while(true)
-		{
-			std::cout << " Es gibt " << stones << " Steine. Bitte nehmen Sie 1,2 oder 3!" << std::endl;
-			std::cin >> move;
-			if (move >= 1 && move <= 3) break;
-			std::cout << "Ungueltiger Zug!";
-		}
-		
+		execute_move();
 		terminate_move("Spieler");
 		
 	}
 
+	void execute_move()
+	{
+		do
+		{
+			players_move();
+		} while (move_is_not_valid());
+		
+		
+	}
+
+	void players_move()
+	{
+		print("Es gibt " + std::to_string(stones) + " Steine. Bitte nehmen Sie 1,2 oder 3!");
+		std::cin >> move;
+	}
+
+	
+
+	bool move_is_not_valid() const
+	{
+		if (is_valid()) return false;
+		print("Ungueltiger Zug!");
+		return true;
+	}
 
 
-
-	void terminate_move(std::string player_name)
+	void terminate_move(const std::string &player_name)
 	{
 		update_game_state();
 		show_gameover_message_when_game_is_over(player_name);
 	}
-	
-	void show_gameover_message_when_game_is_over(std::string player_name)
+	void show_gameover_message_when_game_is_over(const std::string &player_name)
 	{
 		if (is_gameover())
 		{
-			std::cout << player_name << " hat verloren" << std::endl;
+			print(player_name + " hat verloren");
 		}
 	}
-	
+	bool is_valid() const
+	{
+		return move >= 1 && move <= 3;
+	}
 	void update_game_state()
 	{
 		stones -= move;
+	}
+
+	void print(const std::string &message) const
+	{
+		std::cout << message << std::endl;
 	}
 	
 public:
